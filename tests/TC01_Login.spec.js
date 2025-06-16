d71262a6791ed3db7650eb64b06b65f0b5b66483
Ankita Singhai <anki.singhai29@gmail.com>
2025-06-16 15:22:19 +0530

    updated

--- tests/TC01_Login.spec.js
+++ tests/TC01_Login.spec.js
@@ -12,6 +12,7 @@ test.beforeEach('Launching the website',async({page})=>{
 
 
 test('Validate user is able to login into app with valid credential',async({page})=>{
+    //we save credential in .env file so to access its data use - process.env.VARIABLENAME
     const username = process.env.USER;
     const password = process.env.PASSWORD;
    await login.getLoginIntoApp(username,password);
@@ -26,14 +27,14 @@ test.fail('validate user is unable to login into app with blank credential field
 
 test.fail('Validate user is unable to login into app with invalid username',async({page})=>{
     const username = Math.random().toString(36).substring(2, 12);
-    await login.getLoginIntoApp(username,process.env.password);
+    await login.getLoginIntoApp(username,process.env.PASSWORD);
     await login.getWarningMessage();
     await expect(page).toHaveTitle('My Account');
 })
 
 test.fail('Validate user is unable to login into app with invalid password',async({page})=>{
     const password = Math.random().toString(36).substring(2, 12);
-    await login.getLoginIntoApp(process.env.username,password);
+    await login.getLoginIntoApp(process.env.USER,password);
     await login.getWarningMessage();
     await expect(page).toHaveTitle('My Account');
 })
